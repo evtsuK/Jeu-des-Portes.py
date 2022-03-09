2864cc99230791a7c5a71c2be01d9e1290595bbf
evtsuK <samitwitchtv@gmail.com>
2022-03-08 23:15:19 -0500

    first commit

---
+++ portes.py
@@ -0,0 +1,23 @@
+import random
+
+
+print('Bienvenue dans un jeu ou tu dois trouver la pomme cachée derrière une des portes')
+
+nbPortes=60
+while nbPortes<0 or nbPortes>6:
+    nbPortes = int(input('Combien de portes voulez-vous ajouter ? :'))
+    if 0<nbPortes<=6 :
+        tabPortes = ['-' for i in range(nbPortes)]
+        posPomme = random.randint(0,nbPortes)
+        tabPortes[posPomme]='pomme'
+        choix = 8 
+        while choix > nbPortes:
+            print(tabPortes)
+            choix = int(input('Quel est la porte que vous souhaitez choisir : '))
+            if choix == tabPortes[posPomme]:
+                print('Félicitation vous avez réussis')
+            else:
+                print('Veuillez-reessayé')
+    else:
+        print('veuillez-choisir une valeur entre 0 et 6')
+
